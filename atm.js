2b862a0b45bb8093f6be3c24bf424d3c5a5bebd4
ATM-b <temitopemichael619@gmail.com>
2025-07-29 23:39:12 -0700

    Initial commit

---
+++ atm.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const list = document.querySelector(".list");
+
+  hamburger.addEventListener("click", () => {
+    list.classList.toggle("active");
+  });
+});
+ 

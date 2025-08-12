42a26dc9de8fc6aa67a34ce350fe4992605d0c74
ATM-b <temitopemichael619@gmail.com>
2025-08-11 17:46:44 -0700

    changes

--- atm.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger");
-  const list = document.querySelector(".list");
-
-  hamburger.addEventListener("click", () => {
-    list.classList.toggle("active");
-  });
-});
- 
